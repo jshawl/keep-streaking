262b010835f76b4e2f40022bbc289aac228934d2
Jesse shawl <jesse@jshawl.com>
2014-05-28 07:06:22 -0400

    Check user in db? with callback

--- routes/index.js
+++ routes/index.js
@@ -13,38 +13,47 @@ router.get('/', function(req, res) {
       title: 'Keep Streaking'});
 });
 
-router.get('/:username', function(req, res){
+function user_in_db(user, callback){
   var client = new pg.Client(conString);
   client.connect(function(err) {
     if(err) {
       return console.error('could not connect to postgres', err);
     }
-    client.query("SELECT * FROM streakers WHERE username = '"+ req.params.username +"';", function(err, result) {
+    client.query("SELECT * FROM streakers WHERE username = '"+ user +"';", function(err, result) {
       if(err) {
 	return console.error('error running query', err);
       }
-      console.log(result.rows);
-      if (result.rows.length == 0){
-        res.render('error', {user: req.params.username }); 
+      if (result.rows.length == 0){ //if user is not in db
+	return callback(false);
       }
-      //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+      return callback(true);
       client.end();
     });
   });
-  url = 'https://github.com/' + req.params.username;
-  request(url, function(err, resp, body){
-    $ = cheerio.load( body );
-    num = $('.contrib-streak-current .num').text();
-    console.log(num);
-    last_day = $('.contrib-streak-current').text().split('-')[1].replace(/(?!\w)[\x00-\xC0]/g, '');
-    today = strftime('%B%d').replace(/(?!\w)[\x00-\xC0]/g, '');
-    is_today = today == last_day ? true : false;
-    res.render('streaker', { 
-      title: 'Keep Streaking',
-      current_streak: num,
-      user: req.params.username,
-      is_today: is_today });
-    });
+}
+
+router.get('/:username', function(req, res){
+
+  user_in_db( req.params.username, function(  bool ){
+    if (bool){
+    url = 'https://github.com/' + req.params.username;
+    request(url, function(err, resp, body){
+      $ = cheerio.load( body );
+      num = $('.contrib-streak-current .num').text();
+      last_day = $('.contrib-streak-current').text().split('-')[1].replace(/(?!\w)[\x00-\xC0]/g, '');
+      today = strftime('%B%d').replace(/(?!\w)[\x00-\xC0]/g, '');
+      is_today = today == last_day ? true : false;
+      res.render('streaker', { 
+	title: 'Keep Streaking',
+	current_streak: num,
+	user: req.params.username,
+	is_today: is_today });
+      });
+
+    } else {
+      res.render('error', {user: req.params.username});
+    }
+  });
 })
 
 module.exports = router;
