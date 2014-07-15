3366c8e63bc77bdd2ebf8cfc3a6d90555183307c
Jesse Shawl <jesse@jshawl.com>
2014-07-15 16:25:22 -0400

    add errors in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.consider_all_requests_local = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -11,7 +12,6 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
