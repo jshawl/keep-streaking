e7675f7f14107b7041563df0d687b2ee15e0bf22
Jesse Shawl <jesse@jshawl.com>
2014-07-18 13:43:21 -0400

    fix logout view

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -2,6 +2,6 @@ class LeaderboardController < ApplicationController
   def index
     @streaks = Streak.leaderboard
     @settings = Setting.all
-    @setting = current_user.setting || Setting.new
+    @setting = current_user ? current_user.setting : Setting.new
   end
 end
