e6f77e83d68ed86e2fd169134ff9cc2ea30118a9
Jesse Shawl <jesse@jshawl.com>
2014-07-25 17:32:30 -0400

    add error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,9 @@ class ApplicationController < ActionController::Base
    
   private
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    begin
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue
+    end
   end
 end
