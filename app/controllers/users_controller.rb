bd844f2acf50366b4c6f8dc9f4c048a1421353dc
Jesse Shawl <jesse@jshawl.com>
2014-08-01 15:54:07 -0400

    add redirect if logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
     def index
       @users = User.all().length
       @topstreak = Streak.top_streak
+      redirect_to '/leaderboard' if current_user
     end
 end
