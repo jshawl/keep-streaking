7311839f468f6659809999bac1458c7425d2db99
Jesse Shawl <jesse@jshawl.com>
2014-08-01 15:57:43 -0400

    allow leaderboard view if not logged in

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -2,7 +2,7 @@ class LeaderboardController < ApplicationController
   def index
     @streaks = Streak.leaderboard
     @settings = Setting.all
-    if current_user.setting
+    if current_user && current_user.setting
       @setting = current_user.setting 
     else
       @setting = Setting.new
