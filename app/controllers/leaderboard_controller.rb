569e315136c84e9a508dffbf700e3d13fb8fcb67
Jesse Shawl <jesse@jshawl.com>
2014-07-31 22:00:51 -0400

    fix? leaderboard new user

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -2,6 +2,10 @@ class LeaderboardController < ApplicationController
   def index
     @streaks = Streak.leaderboard
     @settings = Setting.all
-    @setting =  current_user.setting || Setting.new
+    if current_user.setting
+      @setting = current_user.setting 
+    else
+      @setting = Setting.new
+    end
   end
 end
